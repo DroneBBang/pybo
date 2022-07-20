8ebcf39af364e5d82267ac27541f30742e847e6b
유정수 <yjs3216@naver.com>
2022-07-20 11:55:02 +0900

    settings 변경 debug off in web

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,8 @@
 from .base import *
 
+# AWS에서의 접속 관련
+
 ALLOWED_HOSTS = ['13.124.30.106']
 STATIC_ROOT = BASE_DIR / 'static/'                  #  /home/ubuntu/projects/mysite/static 등록
 STATICFILES_DIRS = []                               # base.py에 정의된 것 가져옴.
+DEBUG = False                                       # 오류발생시 디버그모드(코드노출)를 막는다.
