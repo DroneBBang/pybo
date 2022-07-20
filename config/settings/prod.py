b48a428eaf9fa383803ebe498b98a8b6da479f1c
유정수 <yjs3216@naver.com>
2022-07-20 10:50:28 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.124.30.106']
+STATIC_ROOT = BASE_DIR / 'static/'                  #  /home/ubuntu/projects/mysite/static 등록
+STATICFILES_DIRS = []                               # base.py에 정의된 것 가져옴.
